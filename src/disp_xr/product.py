bddd3f6467897708f3b2a8d031b41d95c265d839
mgovorcin <mgovorcinzd@gmail.com>
2025-02-06 19:17:25 -0800

    add product and stack modules

---
+++ src/disp_xr/product.py
@@ -0,0 +1,86 @@
+import logging 
+import pandas as pd
+from pathlib import Path
+from typing import List, Union
+
+logger = logging.getLogger(__name__)
+
+def _get_filename_info(products_path: Path) ->pd.DataFrame:
+    """
+    Get filename information from OPERA DISP products.
+
+    Args:
+        products_path (Path): The path to the OPERA DISP products.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the filename information.
+        list: List of duplicate DISP products
+        reference_dates: List of reference dates
+
+    """
+    # Get all OPERA DISP products in the specified path
+    disp_products = list(Path(products_path).rglob('*.nc'))
+    logger.info(f'Found OPERA DISP: {len(disp_products)} products')
+
+    # Read the metadata from the filenames
+    disp_df = pd.DataFrame([product.stem.split('_') for product in disp_products],
+                            columns=['project', 'level', 'product', 'mode', 'frame_id',
+                                     'polarizarion', 'start_date', 'end_date', 'version',
+                                     'production_date'])
+    
+    # Add path, first and second date
+    disp_df['path'] = disp_products
+    disp_df['date12'] = [f'{d.start_date.split("T")[0]}_{d.end_date.split("T")[0]}'
+                        for i, d in  disp_df.iterrows()]
+    disp_df['date1'] = [f'{d.start_date.split("T")[0]}' for i, d in  disp_df.iterrows()]
+    disp_df['date2'] = [f'{d.end_date.split("T")[0]}' for i, d in  disp_df.iterrows()]
+
+    # Dates string to datetime
+    format = '%Y%m%dT%H%M%SZ'
+    disp_df['start_date'] = pd.to_datetime(disp_df['start_date'], format=format)
+    disp_df['end_date'] = pd.to_datetime(disp_df['end_date'], format=format)
+    logger.info(f' Starting date: {disp_df.start_date.min()}')
+    logger.info(f' Ending date: {disp_df.end_date.max()}')
+
+    # Get number of reference dates
+    logger.info(f' Number of reference dates: {len(_get_ministacks(disp_df)[1])}')
+
+    return _find_duplicates(disp_df)[0] 
+
+def _get_ministacks(df: pd.DataFrame) -> Union[pd.DataFrame, List]:
+    mini_stacks = df.groupby(['date1', 'date2']).apply(lambda x: x, include_groups=False)
+    reference_dates = mini_stacks.index.get_level_values(0).unique()
+    return mini_stacks, reference_dates
+
+def _find_duplicates(input_df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Find and remove duplicates in the input DataFrame based on the 'date12' column.
+
+    Args:
+        input_df (pandas.DataFrame): The input DataFrame containing the data.
+
+    Returns:
+        tuple: A tuple containing two DataFrames. The first DataFrame is the input DataFrame
+        with the duplicates removed, and the second DataFrame contains the removed duplicate rows.
+
+    """
+    input_df2 = input_df.copy()
+    list_duplicates = input_df2.date12.value_counts() 
+    duplicates = list_duplicates[list_duplicates.values > 1].index
+
+    duplicate_list = []
+    for date in duplicates:
+        selected_df = input_df2[input_df2.date12 == date]
+        latest_production_date = selected_df.production_date.max()
+        for ix, key in (selected_df.production_date != latest_production_date).items():
+            if key == True:
+                input_df2.drop(ix, inplace=True)
+            else:
+                duplicate_list.append(input_df.iloc[ix])
+    
+    if len(duplicate_list) > 0:
+        duplicate_list = pd.concat(duplicate_list, axis=1).T
+    
+    logger.info(f' Skip {len(duplicate_list)} duplicates')
+
+    return input_df2, duplicate_list
---
+++ src/disp_xr/stack.py
@@ -0,0 +1,43 @@
+import logging
+from typing import List
+import xarray as xr
+import pandas as pd
+
+from .product import _get_ministacks
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_CHUNKS = {'time':-1, 'x':128, 'y':128} 
+
+import pandas as pd
+import xarray as xr
+
+def combine_disp_product(disp_df: pd.DataFrame, chunks: dict = None) -> xr.Dataset:
+    """Stacks displacement products over time.
+
+    Args:
+        disp_df (pd.DataFrame): DataFrame containing displacement file paths with 'date1' and 'date2'.
+        chunks (dict, optional): Chunking configuration for xarray. Defaults to DEFAULT_CHUNKS.
+
+    Returns:
+        xr.Dataset: Stacked displacement dataset.
+    """
+    chunks = {**DEFAULT_CHUNKS, **(chunks or {})}  # Merge default chunks with user-defined chunks
+
+    # Get ministack and reference dates
+    mini_stacks, reference_dates = _get_ministacks(disp_df)
+
+    stacks = []
+    for ix, date in enumerate(reference_dates):
+        stack_files = mini_stacks.loc[date].sort_index().path.to_list()
+        stack = xr.open_mfdataset(stack_files, chunks=chunks)
+        
+        # Append first epcoh of new ministack to last epochs of previous
+        if ix > 0:
+            stack['displacement'] += stacks[ix - 1].displacement.isel(time=-1)
+
+        stacks.append(stack)
+
+    return xr.concat(stacks, dim='time')
+
+
